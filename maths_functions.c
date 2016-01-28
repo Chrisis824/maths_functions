b5f08c1dd2dacf52a8eb75a5805fb02f435e7651
Chrysanthos Konstantinidis <P14149479@GWHXX584LIN018W.lec-admin.dmu.ac.uk>
2016-01-28 15:40:51 +0000

    the maths functions file

---
+++ maths_functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 5;
+	int y = 8;
+	int max = 5;
+	int min = 1;
+	printf ("%i * %i = %i\n", x ,y, x+y);
+	printf ("%i * %i = %i\n", x ,y, x-y);
+	printf ("%i * %i = %i\n", x ,y, x*y);
+	printf ("%i * %i = %i\n", x ,y, x/y);
+	printf("That's all\n");
+	
+	printf("Enter a number\n");
+	return 0;
+}
