8dddb5c1420efa6b8b89b315e303e9f9cd26b0db
Chrysanthos Konstantinidis <p14149479@myemail.dmu.ac.uk>
2016-02-01 22:53:21 +0000

    inserted the 2nd line in the master branch

--- maths_functions.c
+++ maths_functions.c
@@ -3,6 +3,8 @@
 =======
 //This is the line causing issue in the conflict branch.
 >>>>>>> conflict
+
+//This is the second line causing issue in the <master> branch
 #include <stdio.h>
 
 int main()
