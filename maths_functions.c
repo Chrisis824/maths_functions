28b057b0d7fe4d48e1eee491a52cdb551fb54f5c
Chrysanthos Konstantinidis <p14149479@myemail.dmu.ac.uk>
2016-02-04 17:11:03 +0000

    edited maths_functions.c in testing branch

--- maths_functions.c
+++ maths_functions.c
@@ -21,3 +21,35 @@ int main()
 	return 0;
 	
 }
+
+int lab4() {
+    int a[5];
+    int i;
+    int min;
+    int max;
+    printf("Enter five values:");
+    //Store 10 numbers in an array
+    for (i = 0; i < 5; i++) {
+    	scanf("%d", &a[i]);
+    }
+    //Assume that a[0] is minimum
+    max = a[0];
+    for (i = 0; i < 5; i++) {
+	if (a[i] > max) {
+	max = a[i];
+    }
+    }
+    printf("Maximum of five numbers is %d", max);
+    return 0;
+
+   //Assume that a[0] is minimum
+   min = a[0];
+   for (i=0; i<5; i++){
+   if (a[i] > min) {
+   min = a[i];
+   }
+   }
+   printf("\nMinimum of five numbers is %d", min);
+    return 0;
+  }
+
